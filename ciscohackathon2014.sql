256a12b1c3d4719fb725f675fe967309e9feec3c
biego <gxb319@bham.ac.uk>
2014-10-24 00:06:00 +0100

    More sql

--- ciscohackathon2014.sql
+++ ciscohackathon2014.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2014 at 11:23 PM
+-- Generation Time: Oct 24, 2014 at 01:04 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -83,7 +83,12 @@ INSERT INTO `emergencies` (`eid`, `location`, `time`, `phone_nr`, `cid`) VALUES
 (2, 'University station', '2014-08-17', 771117546, 3),
 (3, 'Selly Oak Station', '0000-00-00', 771337546, 4),
 (4, 'Five Ways', '0000-00-00', 771117546, 1),
-(5, 'Random station', '0000-00-00', 770017546, 2);
+(5, 'Longbridge station', '2014-07-13', 770017546, 2),
+(6, 'Coventry station', '2014-10-11', 2147483647, 4),
+(7, 'New Street station', '2014-03-11', 2147483647, 2),
+(8, 'University station', '2014-06-11', 2147483647, 2),
+(9, 'Selly Oak station', '2014-10-11', 2147483647, 1),
+(10, 'University station', '2014-06-22', 2147483647, 4);
 
 -- --------------------------------------------------------
 
@@ -104,7 +109,13 @@ CREATE TABLE IF NOT EXISTS `evidence` (
 --
 
 INSERT INTO `evidence` (`eid`, `name`, `source`, `type`, `pid`) VALUES
-(1, 'Jane Doe', 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wC', 'Photo', 1);
+(1, 'Jane Doe', 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wC', 'Photo', 1),
+(2, 'Surveillance camera #03', 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBhQS', 'Photo', 1),
+(3, 'Surveillance camera #52', 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABA', 'Video', 2),
+(4, 'Surveillance camera #32', 'data:image/jpeg;base64,/9j/4AAQSkZJ4521354321354A', 'Video', 2),
+(5, 'Surveillance camera #11', 'data:image/jpeg;base64,/9j/4AA/654321KSJHDFKSJDFHKL', 'Video', 2),
+(6, 'Surveillance camera #03', 'data:video/mp4;base64,/9j/4AAQSkZJRgABAQAAAQABA', 'Video', 7),
+(8, 'Surveillance camera #05', 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABA', 'Image', 7);
 
 -- --------------------------------------------------------
 
@@ -140,7 +151,11 @@ CREATE TABLE IF NOT EXISTS `police_report` (
 INSERT INTO `police_report` (`pid`, `date`, `cid`, `location`, `describtion`) VALUES
 (1, '2014-10-10', 3, 'New Street Station', 'A group of students arrested for drinking on the public transport'),
 (2, '2014-10-19', 4, 'Selly Oak station', 'Former IRA Chief of Staff was spotted stealing metal railings on the train'),
-(3, '2014-08-11', 2, 'University Station', 'A student reported a stolen laptop as he was taking a train to London from Edinburgh');
+(3, '2014-08-11', 2, 'University Station', 'A student reported a stolen laptop as he was taking a train to London from Edinburgh'),
+(4, '2014-07-20', 1, 'New Street station', 'Surveillance camera has detected a group of young adults destroying the train propperty'),
+(5, '2014-08-03', 2, 'University station', 'A student has reported his wallet stolen'),
+(6, '2014-03-03', 2, 'Longbridge station', 'Tom Sammons has reported a stolen laptop'),
+(7, '2014-07-07', 4, 'New Street station', 'Cable theft has been reported by the train staff');
 
 -- --------------------------------------------------------
 
@@ -177,7 +192,13 @@ CREATE TABLE IF NOT EXISTS `witness_report` (
 INSERT INTO `witness_report` (`wid`, `pid`, `date`, `location`, `statement`) VALUES
 (1, 1, '2014-10-11', 'New Street station', 'Jane Doe has reported a group of students drinking and shouting on the train going from University station to New Street station'),
 (2, 1, '2014-10-12', 'Five Ways', 'Dan Ghica has identified three of University of Birmingham students that were accused of drug abuse on the train from University station to New Street'),
-(3, 2, '2014-10-23', 'Selly Oak station', 'Poppie Simmonds has reported seeing a man in his 50-ies stealing metal railings from the train going towards Selly Oak station');
+(3, 2, '2014-10-23', 'Selly Oak station', 'Poppie Simmonds has reported seeing a man in his 50-ies stealing metal railings from the train going towards Selly Oak station'),
+(4, 2, '2014-10-19', 'Selly Oak station', 'Thomas Sammonds has reported a man stealing metal railings on the train'),
+(5, 2, '2014-10-20', 'Selly Oak station', 'Diana Murgulet has reported a man stealing metal railings on the train'),
+(6, 2, '2014-10-19', 'Selly Oak station', 'Helen Whitby has reported a man stealing metal railings on the train'),
+(7, 3, '2014-08-11', 'University station', 'University of Birmingham student Benjamin Dixon has reported that a man in his 20-ies grabbed his back and started running, ran out of the train and out of the station'),
+(8, 3, '2014-08-11', 'University station', 'A friend of the victim - Poppie Simmonds has reported that a man in his 20-ies grabbed his back and started running, ran out of the train and out of the station '),
+(9, 7, '2014-07-07', 'New Street station', 'A member of the train staff - Mark Lee has reported that cables from the trainhas gone missing');
 
 --
 -- Indexes for dumped tables
