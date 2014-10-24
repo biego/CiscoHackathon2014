5cb88dc4b6acf0a09006ca0fc91c2e5f32d9348c
gweennnnn <me@gwendolyn.info>
2014-10-24 05:06:05 +0100

    your face needs me to lift it

---
+++ Website/test.php
@@ -0,0 +1,16 @@
+<?php
+include_once("connection/functions.php"); ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	echo "MOOP!<br/>";
+	showAllRelatedEvidence(1);
+?>
+</body>
+</html>
