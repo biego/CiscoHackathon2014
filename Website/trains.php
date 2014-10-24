5583d06e7a7951a163537c61f89d4780fdc50591
Diana Murgulet <dianamurgulet@gmail.com>
2014-10-24 07:30:04 +0100

    trains

--- Website/trains.php
+++ Website/trains.php
@@ -1,19 +1,28 @@
+<!doctype html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="theme/javascript/javascript.js"></script>
+<meta charset="utf-8">
+<title>TrainWatch!</title>
 <link rel="stylesheet" href="css/reset.css" type="text/css"/>
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
-<title>TrainWatch!</title>
-</head>	
+</head>
 
 <body>
-<div id="trainInfoContainer"></div>
+	<div id = "outer"> <!-- every DIV from now own should have class="inner"-->
+		<div class= "inner"> <!-- every item (img, text, etc) should have its individual encaseing div. Guarantees resizing. -->
+			<div id="title" class="inner"> 
+            	<img src="img/LOGO Train watch.png" class="innercontent"/>  <!-- every ITEM should have class="innercontent"-->
+            </div>
+            
+            <!-- Insert data here. -->
+			<div id="trainInfoContainer"></div>
 <script type="text/javascript" src="javascript/request_data.js"></script>
 <script type="text/javascript" src="javascript/liveTrains.js"></script>
 
 <script type="text/javascript">
 	requestData("javascript/data/trainInfo.html",callBack);
 </script>
+        </div>
+    </div>
 </body>
 </html>
---
+++ Website/trainsbackup.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="theme/javascript/javascript.js"></script>
+<link rel="stylesheet" href="css/reset.css" type="text/css"/>
+<link rel="stylesheet" href="css/style.css" type="text/css"/>
+<title>TrainWatch!</title>
+</head>	
+
+<body>
+<div id="trainInfoContainer"></div>
+<script type="text/javascript" src="javascript/request_data.js"></script>
+<script type="text/javascript" src="javascript/liveTrains.js"></script>
+
+<script type="text/javascript">
+	requestData("javascript/data/trainInfo.html",callBack);
+</script>
+</body>
+</html>
